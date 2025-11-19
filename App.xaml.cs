bd05c6874d4eb987c0d03b3111bb083eb1b87693
sayo <nogami8810@icloud.com>
2025-11-20 02:15:48 +0900

    テンプレート作成

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SerialApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
